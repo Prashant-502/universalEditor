0c7f949ce87fa571d1e0cb88676f80b7e81fd4c6
Prashant-0207 <prashantshigvanofficial@gmail.com>
2024-08-07 17:10:45 +0530

    carousel

--- blocks/listitem/listitem.js
+++ blocks/listitem/listitem.js
@@ -1,12 +1,12 @@
-export function generateLiHtmml(block){
-  var blockChildren = block.querySelector('div');
-  const props = Array.from(blockChildren.children, (row) => row);
-  const [image, text] = props;
+// export function generateLiHtmml(block){
+//   var blockChildren = block.querySelector('div');
+//   const props = Array.from(blockChildren.children, (row) => row);
+//   const [image, text] = props;
 
-  const liblock = document.createElement('li');
-  liblock.innerHTML = image.innerHTML;
-  liblock.innerHTML += text.innerHTML;
+//   const liblock = document.createElement('li');
+//   liblock.innerHTML = image.innerHTML;
+//   liblock.innerHTML += text.innerHTML;
 
-  return liblock;
+//   return liblock;
 
-}
+// }
