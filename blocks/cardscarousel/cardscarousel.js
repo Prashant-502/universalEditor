2c8f172a0240ebac11142c2b0b0844f700aeaeb4
Prashant-0207 <prashantshigvanofficial@gmail.com>
2024-08-08 13:07:59 +0530

    cardscarousel

--- blocks/cardscarousel/cardscarousel.js
+++ blocks/cardscarousel/cardscarousel.js
@@ -2,6 +2,8 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+    block.classlist.add("glide__track");
+    console.log(block);
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
