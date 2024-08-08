cb604bb499ebb9b9c893395e95e6f9382c992c0e
Prashant-0207 <prashantshigvanofficial@gmail.com>
2024-08-08 12:51:45 +0530

    cardscarousel

---
+++ blocks/ul/ul.js
@@ -0,0 +1,11 @@
+import { generateLiHtml } from "../listitem/listitem.js";
+
+export function decorate(block) {
+    const ulblock = document.createElement('ul');
+    
+    const liBlock = generateLiHtml(block);
+    ulblock.append(liBlock);
+
+    block.innerHTML = ""
+    block.insertAdjacentElement('afterbegin', ulblock);
+}
