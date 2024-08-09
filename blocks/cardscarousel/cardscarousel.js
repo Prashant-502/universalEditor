4b266aea602d3a15796cc85330c2a21fad8ae900
Prashant-0207 <prashantshigvanofficial@gmail.com>
2024-08-09 13:12:40 +0530

    css slider

--- blocks/cardscarousel/cardscarousel.js
+++ blocks/cardscarousel/cardscarousel.js
@@ -1,12 +1,13 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import Glide from './glide.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  block.parentElement.classList.add('glide');
-  block.parentElement.classList.add('carouselinitializer');
-  block.classList.add('glide__track');
-  block.setAttribute("data-glide-el", "track");
+  // block.parentElement.classList.add('glide');
+  // block.parentElement.classList.add('carouselinitializer');
+  // block.classList.add('glide__track');
+  // block.setAttribute("data-glide-el", "track");
   const ul = document.createElement('ul');
   ul.classList.add('glide__slides');
   [...block.children].forEach((row) => {
@@ -26,5 +27,29 @@ export default function decorate(block) {
     img.closest('picture').replaceWith(optimizedPic);
   });
   block.textContent = '';
-  block.append(ul);
+  const div = document.createElement('div');
+  div.classList.add('glide__track')
+  div.setAttribute('data-glide-el', 'track')
+  div.append(ul)
+  block.classList.add('glide','carousel')
+  block.append(div);
+
+  // initializer
+  const slideObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        var glideHero =new Glide('.carousel', {
+          type: 'carousel',
+          animationDuration: 2000,
+          autoplay: 4500,
+          focusAt: '1',
+          startAt: 1,
+          perView: 1,
+        });
+        glideHero.mount();
+
+      }
+    });
+  });
+  slideObserver.observe(block);
 }
